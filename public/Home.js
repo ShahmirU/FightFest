76592c871af53f4d12d2eca12761105958917547
Shahmir Usman <43623854+ShahmirU@users.noreply.github.com>
2019-05-25 04:44:00 +0400

    Updated client side to display all divisions.

--- public/Home.js
+++ public/Home.js
@@ -4,15 +4,30 @@ function populate(s1, s2, s3){
     var s3 = document.getElementById(s3);
     s2.innerHTML = "";
     s3.innerHTML = "";
-    if(s1.value == "Heavyweight"){
-        var optionArray = ["|", "danielcormier|Daniel Cormier"]
-    }
+    var optionArray = [];
+
+    $(document).ready(function () {
+        $.get('/'+s1.value.toLowerCase().replace(" ", "").replace("'", ""), function(data){
+            if(!data){
+                console.log("Unable to obtain fighter details");
+                alert("Error: Unable to obtain fighter details");
+            }
+            else{
+                optionArray = data;
+                optionArray.unshift("|");
+                pairToOption(optionArray, s2, s3);
+            }
+        })
+    });    
+}
+
+function pairToOption(optionArray, s2, s3){
     for (var option in optionArray){
         var pair = optionArray[option].split("|");
         var newOption1 = document.createElement("option");
         var newOption2 = document.createElement("option");
-        newOption1.value = newOption2.value = pair[0];
-        newOption1.innerHTML = newOption2.innerHTML = pair[1];
+        newOption1.value = newOption2.value = pair[1];
+        newOption1.innerHTML = newOption2.innerHTML = pair[0];
         s2.options.add(newOption1);
         s3.options.add(newOption2);
     }
